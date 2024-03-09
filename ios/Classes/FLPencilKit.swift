16a7ddf6742adc25f0d401354044bd545bd81629
fakhanvarf1 <fakhnavarf@gmail.com>
2024-03-09 17:15:51 +0300

    1

--- ios/Classes/FLPencilKit.swift
+++ ios/Classes/FLPencilKit.swift
@@ -327,7 +327,7 @@ private class PencilKitView: UIView {
 
   func getBase64Data() -> Any {
     // canvasView.drawing.dataRepresentation().base64Data()
-    let drawingImage =  canvasView.drawing.image(from: rect, scale: scale)
+    let drawingImage =  canvasView.drawing.image(from: CGRect(x: 0, y: 0, width: 512, height: 512), scale: 1/1)
     let data = drawingImage.pngData()
     return data;
   }
